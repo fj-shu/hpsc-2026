713a09f27cd84bc5c5b9e3f03d67229962a8da27
Fangjie Shu <austin@AustindeMacBook-Air.local>
2026-05-10 13:20:57 +0900

    cuda homework

--- 06_cuda/13_bucket_sort.cpp
+++ 06_cuda/13_bucket_sort.cpp
@@ -1,32 +1,66 @@
 #include <cstdio>
 #include <cstdlib>
-#include <vector>
+
+__global__ void clearBucket(int *bucket, int range) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < range)
+    bucket[i] = 0;
+}
+
+__global__ void fillBucket(int *key, int *bucket, int n) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n)
+    atomicAdd(&bucket[key[i]], 1);
+}
+
+__global__ void scanBucket(int *bucket, int *offset, int range) {
+  if (blockIdx.x == 0 && threadIdx.x == 0) {
+    int sum = 0;
+    for (int i=0; i<range; i++) {
+      offset[i] = sum;
+      sum += bucket[i];
+    }
+  }
+}
+
+__global__ void fillKey(int *key, int *bucket, int *offset, int range) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i >= range)
+    return;
+
+  int j = offset[i];
+  for (int count=0; count<bucket[i]; count++)
+    key[j + count] = i;
+}
 
 int main() {
   int n = 50;
+  int block_size = 256;
   int range = 5;
-  std::vector<int> key(n);
+  int *key, *bucket, *offset;
+
+  cudaMallocManaged(&key, n*sizeof(int));
+  cudaMallocManaged(&bucket, range*sizeof(int));
+  cudaMallocManaged(&offset, range*sizeof(int));
+
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
-    bucket[i] = 0;
-  }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
-  }
+  clearBucket<<<(range+block_size-1)/block_size, block_size>>>(bucket, range);
+  fillBucket<<<(n+block_size-1)/block_size, block_size>>>(key, bucket, n);
+  scanBucket<<<1, 1>>>(bucket, offset, range);
+  fillKey<<<(range+block_size-1)/block_size, block_size>>>(key, bucket, offset, range);
+  cudaDeviceSynchronize();
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
   }
   printf("\n");
+
+  cudaFree(key);
+  cudaFree(bucket);
+  cudaFree(offset);
 }
